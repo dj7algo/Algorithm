3ca9e8ef62caccbfab916b06f210a04451fa6f18
Jeongyeon Park <38120577+ParkJYeon@users.noreply.github.com>
2022-03-10 20:41:01 +0900

    10회차 풀이 - 매직스타 수정

--- 백준/10회차/3967(매직스타)/BOJ3967.java
+++ 백준/10회차/3967(매직스타)/BOJ3967.java
@@ -1,43 +1,73 @@
 import java.io.*;
 import java.util.*;
-
-public class BOJ3967 {
-    int[][][] magicstar = new int[][][]{
-            {{0, 4}, {1, 3}, {2, 2}, {3, 2}},
-            {{1, 1}, {1, 3}, {1, 5}, {1, 7}},
-            {{0, 4}, {1, 5}, {2, 6}, {3, 7}},
-            {{1, 1}, {2, 2}, {3, 3}, {4, 4}},
-            {{1, 7}, {2, 6}, {3, 5}, {4, 4}},
-            {{4, 1}, {4, 3}, {4, 5}, {4, 7}}
-    };
+public class Main {
+    static char[] line;
+    static boolean[] alphabet;
+    static boolean isDone;
     public static void main(String[] args) throws Exception {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        boolean[] alphabet = new boolean[12];
-        char[][] board = new char[5][9];
+
+        int len= 0;
+        int idx = 0;
+        line = new char[12];
+        alphabet = new boolean[12];
+        isDone = false;
+        for (int i = 0; i < 5; i++) {
+            String l = br.readLine();
+            for (int j = 0; j < l.length(); j++) {
+                if(l.charAt(j) != '.'){
+                    line[len++] = l.charAt(j);
+                    if(0 <= (int)l.charAt(j) - 'A' && (int)l.charAt(j) - 'A' < 12) {
+                        alphabet[(int) l.charAt(j) - 'A'] = true;
+                        idx++;
+                    }
+                }
+            }
+        }
+        dfs(0);
         br.close();
-
-        for(int i = 0 ; i < 5 ; i++){
-            char[] line = br.readLine().toCharArray();
-            for(int j = 0 ; j < line.length ; i++){
-                board[i][j] = line[j];
-                if('A' <= line[j] && line[j] <= 'L') alphabet[line[j] - 'A'] = true;
-            }
-        }
     }
 
-    static void check(char[][] board, boolean[] alphabet, int idx){
+    static void dfs(int idx) {
+        if (isDone) return;
+        if (idx == 12) {
+            if (check()) {
+                StringBuilder sb = new StringBuilder();
+                sb.append("....").append(line[0]).append("....\n")
+                        .append(".").append(line[1]).append(".").append(line[2]).append(".").append(line[3]).append(".").append(line[4]).append(".\n")
+                        .append("..").append(line[5]).append("...").append(line[6]).append("..\n")
+                        .append(".").append(line[7]).append(".").append(line[8]).append(".").append(line[9]).append(".").append(line[10]).append(".\n")
+                        .append("....").append(line[11]).append("....");
+                System.out.println(sb);
+                isDone = true;
+            }
+            return;
+        }
 
 
-
-        for(int i = 0 ; i < 12 ; i++){
-            if(!alphabet[i]){
+        if(line[idx] != 'x') dfs(idx+1);
+        else{
+            for(int i = 0 ; i < 12 ; i++){
+                if(alphabet[i]) continue;
                 alphabet[i] = true;
-
+                line[idx] = (char) (i + 'A');
+                dfs(idx+1);
+                alphabet[i] = false;
+                line[idx] = 'x';
             }
         }
     }
 
-
+    static boolean check() {
+        if((line[0]-'A') + (line[2] - 'A') + (line[5] - 'A')+(line[7] - 'A') + 4 != 26) return false;
+        if((line[1]-'A') + (line[2] - 'A') + (line[3] - 'A')+(line[4] - 'A') + 4 != 26) return false;
+        if((line[0]-'A') + (line[3] - 'A') + (line[6] - 'A')+(line[10] - 'A') + 4 != 26) return false;
+        if((line[1]-'A') + (line[5] - 'A') + (line[8] - 'A')+(line[11] - 'A') + 4 != 26) return false;
+        if((line[4]-'A') + (line[6] - 'A') + (line[9] - 'A')+(line[11] - 'A') + 4 != 26) return false;
+        if((line[7]-'A') + (line[8] - 'A') + (line[9] - 'A')+(line[10] - 'A') + 4 != 26) return false;
+        return true;
+    }
 
 
 }
+
